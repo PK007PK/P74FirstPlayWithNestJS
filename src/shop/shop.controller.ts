c1b7109c1a7110c4db2f2fe0c2c2b8e954ddee4a
Piotr Krasny <krasny.piotr@gmail.com>
2022-10-27 20:58:05 +0200

    Add type

--- src/shop/shop.controller.ts
+++ src/shop/shop.controller.ts
@@ -1,9 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
+import { GetListOfProductsResponse } from 'src/interfaces/interfaces';
 
 @Controller('shop')
 export class ShopController {
   @Get('/')
-  getListOfProducts() {
+  getListOfProducts(): GetListOfProductsResponse {
     return [
       { name: 'Ogórki kiszone', description: 'Bardzo dobre ogórki', price: 10 },
       { name: 'Seler naciowy', description: 'Smaczny i zdrowy', price: 3 },
