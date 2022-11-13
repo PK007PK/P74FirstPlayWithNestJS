cdd53c495edaebb70a88d37837ca2042e626561b
Piotr Krasny <krasny.piotr@gmail.com>
2022-11-13 11:18:51 +0100

    Excercises with foxes

--- src/fox/fox.controller.ts
+++ src/fox/fox.controller.ts
@@ -1,15 +1,31 @@
-import { Controller, Get, Headers, Ip, Query, Req } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Headers,
+  Inject,
+  Ip,
+  Post,
+  Query,
+  Req,
+} from '@nestjs/common';
 import { Request } from 'express';
+import { CreateFoxDto } from './dto/create-fox.dto';
+import { FoxService } from './fox.service';
 
 @Controller('fox')
 export class FoxController {
+  constructor(
+    @Inject(FoxService) private foxService: FoxService, // <--- Dependency injection
+  ) {}
+
   @Get()
   myFirstAction(
-    @Headers('accept-encoding') encoding: string,
-    @Ip() ip: string,
-    @Query('name') name: string,
-    @Query('surname') surname: string,
-    @Req() request: Request,
+    @Headers('accept-encoding') encoding: string, // <--- Getting a header
+    @Ip() ip: string, // <--- Getting an IP address
+    @Query('name') name: string, // <--- Getting a query parameter
+    @Query('surname') surname: string, // <--- Getting a query parameter
+    @Req() request: Request, // <--- Getting a request object
   ) {
     console.log(encoding, ip);
     console.log(request.headers);
@@ -19,5 +35,12 @@ export class FoxController {
     <p>IP: ${ip}</p>
     <p>Name: ${name}, Surname ${surname}</p>
     `;
+  }
+
+  @Post('/')
+  createFox(
+    @Body() newFox: CreateFoxDto, // <--- Getting a body
+  ) {
+    return this.foxService.createFox(newFox); // <--- Calling a service
   }
 }
